48a6a30f98073e18d71719d9bb1e1932979c6397
sarvesh-03 <sarveshjan18@gmail.com>
2021-09-15 23:34:28 +0530

    fix:move base url to RetrofitInstance

--- DauthSDK/src/main/java/edu/nitt/delta/api/ApiRouteConstants.kt
+++ DauthSDK/src/main/java/edu/nitt/delta/api/ApiRouteConstants.kt
@@ -1,7 +1,6 @@
 package edu.nitt.delta.api
 
 object ApiRouteConstants {
-    const val BASE_URL ="https://auth.delta.nitt.edu"
     const val AUTHORIZE_ROUTE="/authorize"
     const val TOKEN_ROUTE="/api/oauth/token"
     const val KEY_ROUTE="/api/oauth/oidc/key"
--- DauthSDK/src/main/java/edu/nitt/delta/api/RetrofitInstance.kt
+++ DauthSDK/src/main/java/edu/nitt/delta/api/RetrofitInstance.kt
@@ -6,10 +6,11 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
+    private const val BASE_URL ="https://auth.delta.nitt.edu"
     private val logger=HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
     private val okHttpClient =OkHttpClient.Builder().addInterceptor(logger)
 
-    private val retrofit=Retrofit.Builder().baseUrl(ApiRouteConstants.BASE_URL)
+    private val retrofit=Retrofit.Builder().baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .client(okHttpClient.build()).build()
     val api = retrofit.create(DAuthApi::class.java)
