517d7a6137790d645e7c92873497e2a88d21a393
sarvesh-03 <83568494+sarvesh-03@users.noreply.github.com>
2021-09-16 11:21:25 +0530

    feat:add retrofit base class (#2)
    * feat:add retrofit base class
    
    * feat:add retrofit base class
    
    * fix:move base url to RetrofitInstance
    
    * fix:move misc.xml to .gitignore
    
    * fix:delete misc.xml file

---
+++ DauthSDK/src/main/java/edu/nitt/delta/api/ApiRouteConstants.kt
@@ -0,0 +1,8 @@
+package edu.nitt.delta.api
+
+object ApiRouteConstants {
+    const val AUTHORIZE_ROUTE="/authorize"
+    const val TOKEN_ROUTE="/api/oauth/token"
+    const val KEY_ROUTE="/api/oauth/oidc/key"
+    const val USER_ROUTE="/api/resources/user"
+}
---
+++ DauthSDK/src/main/java/edu/nitt/delta/api/RetrofitInstance.kt
@@ -0,0 +1,17 @@
+package edu.nitt.delta.api
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private const val BASE_URL ="https://auth.delta.nitt.edu"
+    private val logger=HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    private val okHttpClient =OkHttpClient.Builder().addInterceptor(logger)
+
+    private val retrofit=Retrofit.Builder().baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttpClient.build()).build()
+    val api = retrofit.create(DAuthApi::class.java)
+}
