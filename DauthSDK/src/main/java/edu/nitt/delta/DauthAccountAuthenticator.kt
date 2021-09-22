2f00ee9dd59fb2e1934b13ab8ed3916b11cb405b
V Pranav Krishna <vpranavkrishna@gmail.com>
2021-09-22 10:06:35 +0530

    Created the account manager

--- DauthSDK/src/main/java/edu/nitt/delta/DauthAccountAuthenticator.kt
+++ DauthSDK/src/main/java/edu/nitt/delta/DauthAccountAuthenticator.kt
@@ -1,10 +1,23 @@
 package edu.nitt.delta
 
+import android.R.attr
 import android.accounts.AbstractAccountAuthenticator
 import android.accounts.Account
 import android.accounts.AccountAuthenticatorResponse
 import android.content.Context
 import android.os.Bundle
+import android.accounts.AccountManager
+
+import android.R.attr.accountType
+
+import android.content.Intent
+import android.text.TextUtils
+
+
+
+
+
+
 
 class DauthAccountAuthenticator(context: Context) : AbstractAccountAuthenticator(context) {
     override fun editProperties(
@@ -21,7 +34,15 @@ class DauthAccountAuthenticator(context: Context) : AbstractAccountAuthenticator
         requiredFeatures: Array<out String>?,
         options: Bundle?
     ): Bundle {
-        TODO("Not yet implemented")
+//        TODO("Not yet implemented")
+        val intent = Intent(mContext, AccountsActivity::class.java)
+        intent.putExtra("YOUR ACCOUNT TYPE", attr.accountType)
+        intent.putExtra("full_access", authTokenType)
+        intent.putExtra("is_adding_new_account", true)
+        intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response)
+        val bundle = Bundle()
+        bundle.putParcelable(AccountManager.KEY_INTENT, intent)
+        return bundle
     }
 
     override fun confirmCredentials(
@@ -38,7 +59,32 @@ class DauthAccountAuthenticator(context: Context) : AbstractAccountAuthenticator
         authTokenType: String?,
         options: Bundle?
     ): Bundle {
-        TODO("Not yet implemented")
+//        TODO("Not yet implemented")
+        val am = AccountManager.get(mContext)
+
+        var authToken = am.peekAuthToken(account, authTokenType)
+
+        if (TextUtils.isEmpty(authToken)) {
+            authToken = HTTPNetwork.login(account!!.name, am.getPassword(account))
+        }
+
+
+        if (!TextUtils.isEmpty(authToken)) {
+            val result = Bundle()
+            result.putString(AccountManager.KEY_ACCOUNT_NAME, account!!.name)
+            result.putString(AccountManager.KEY_ACCOUNT_TYPE, account!!.type)
+            result.putString(AccountManager.KEY_AUTHTOKEN, authToken)
+            return result
+        }
+
+        val intent = Intent(mContext, AccountsActivity::class.java)
+        intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response)
+        intent.putExtra("YOUR ACCOUNT TYPE", account!!.type)
+        intent.putExtra("full_access", authTokenType)
+
+        val retBundle = Bundle()
+        retBundle.putParcelable(AccountManager.KEY_INTENT, intent)
+        return retBundle
     }
 
     override fun getAuthTokenLabel(authTokenType: String?): String {
