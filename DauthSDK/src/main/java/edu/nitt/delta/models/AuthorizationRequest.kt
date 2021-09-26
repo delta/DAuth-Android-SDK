6e6731df2173d63601fcb8a173b239cd0c7015ba
Sudhindra Dondapati <55095527+Sudhindra3000@users.noreply.github.com>
2021-09-26 11:15:35 +0530

    feat(init): Read client credentials from JSON file (#10)
    * create ClientCredentials object from client-creds.json
    
    * remove unnecessary fields from AuthorizationRequest

--- DauthSDK/src/main/java/edu/nitt/delta/models/AuthorizationRequest.kt
+++ DauthSDK/src/main/java/edu/nitt/delta/models/AuthorizationRequest.kt
@@ -1,11 +1,9 @@
 package edu.nitt.delta.models
 
 data class AuthorizationRequest(
-    val client_id:String,
-    val redirect_uri:String,
-    val response_type:ResponseType,
-    val grant_type:GrantType,
-    val state:String,
-    val scopes:List<Scope>,
-    val nonce:String
+    val response_type: ResponseType,
+    val grant_type: GrantType,
+    val state: String,
+    val scopes: List<Scope>,
+    val nonce: String
 )
--- sampleApp/src/main/java/edu/nitt/delta/MainActivity.kt
+++ sampleApp/src/main/java/edu/nitt/delta/MainActivity.kt
@@ -14,21 +14,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val dAuth = DAuth()
         val signInButton: Button = findViewById(R.id.sign_in_button)
         signInButton.setOnClickListener {
-            dAuth.signIn(
+            DAuth.signIn(
                 context = this,
                 authRequest = AuthorizationRequest(
-                    "PwLV_Z_GGJSZECg1",
-                    "https://github.com/",
                     ResponseType.Code,
                     GrantType.AuthorizationCode,
                     "1ww12",
                     listOf(Scope.OpenID, Scope.User, Scope.Email, Scope.Profile),
                     "ncsasd"
                 ),
-                clientSecret = "cRaxHJSugJeo8YJOO5GisYgDv6x53Uxs",
                 signInListener = object : SignInListener {
                     override fun onSuccess(user: User) {
                         println("Success: $user")
