66a4d37f52e16c33e44ec8bdd44e1857b19ad747
Sudhindra <sudhindra5678@gmail.com>
2021-10-12 20:11:05 +0530

    add document comments for RetrofitInstance.kt

--- DauthSDK/src/main/java/edu/nitt/delta/api/RetrofitInstance.kt
+++ DauthSDK/src/main/java/edu/nitt/delta/api/RetrofitInstance.kt
@@ -5,15 +5,32 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+/**
+ * Singleton Object containing the Retrofit api instance
+ */
 internal object RetrofitInstance {
+    /**
+     * Base url of the Delta OAuth2 Service
+     */
     private const val BaseUrl = "https://auth.delta.nitt.edu"
 
+    /**
+     * An [interceptor][HttpLoggingInterceptor] which logs request and response information
+     */
     private val logger = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    /**
+     * [Http client][OkHttpClient] used by retrofit
+     */
     private val okHttpClient = OkHttpClient.Builder().addInterceptor(logger)
 
     private val retrofit = Retrofit.Builder().baseUrl(BaseUrl)
         .addConverterFactory(GsonConverterFactory.create())
-        .client(okHttpClient.build()).build()
+        .client(okHttpClient.build())
+        .build()
 
+    /**
+     * Value Instance of the [DAuthApi] interface
+     */
     val api: DAuthApi = retrofit.create(DAuthApi::class.java)
 }
