10089e0c14e8cc43bc0de6b49ba6da1a74f5649c
Sudhindra <sudhindra5678@gmail.com>
2021-10-12 19:53:45 +0530

    add document comments for ApiRoutes.kt

--- DauthSDK/src/main/java/edu/nitt/delta/api/ApiRoutes.kt
+++ DauthSDK/src/main/java/edu/nitt/delta/api/ApiRoutes.kt
@@ -1,6 +1,16 @@
 package edu.nitt.delta.api
 
+/**
+ * Singleton object containing the api routes
+ */
 internal object ApiRoutes {
+    /**
+     * Token route
+     */
     const val Token = "/api/oauth/token"
+
+    /**
+     * User route
+     */
     const val User = "/api/resources/user"
 }
