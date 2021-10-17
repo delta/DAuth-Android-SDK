7d8da01eeb766830d80aa5dfad475ce5d0884141
Sudhindra3000 <sudhindra5678@gmail.com>
2021-10-17 15:54:50 +0530

    add Doc comments for DateUtils.kt

--- DauthSDK/src/main/java/edu/nitt/delta/helpers/DateUtils.kt
+++ DauthSDK/src/main/java/edu/nitt/delta/helpers/DateUtils.kt
@@ -3,6 +3,16 @@ package edu.nitt.delta.helpers
 import java.text.SimpleDateFormat
 import java.util.*
 
-internal fun Date.toFormatString(format: String) = SimpleDateFormat(format).format(this)
+/**
+ * An extension function to format a [Date] into a date/time string
+ * @param pattern the pattern describing the date and time format
+ * @return The formatted time string
+ */
+internal fun Date.toFormatString(pattern: String) = SimpleDateFormat(pattern).format(this)
 
-internal fun String.toDate(format: String): Date = SimpleDateFormat(format).parse(this)
+/**
+ * An extension function to parse a [Date] from a given date/time string
+ * @param pattern the pattern describing the date and time format
+ * @return The Date parsed from the string
+ */
+internal fun String.toDate(pattern: String): Date = SimpleDateFormat(pattern).parse(this)
