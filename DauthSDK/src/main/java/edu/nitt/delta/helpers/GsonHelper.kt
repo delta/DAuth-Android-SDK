ddcb2def210c249bc11436bb5b4957f189977dc8
Sudhindra3000 <sudhindra5678@gmail.com>
2021-10-18 17:45:17 +0530

    add Doc comments for GsonHelper.kt

--- DauthSDK/src/main/java/edu/nitt/delta/helpers/GsonHelper.kt
+++ DauthSDK/src/main/java/edu/nitt/delta/helpers/GsonHelper.kt
@@ -5,11 +5,15 @@ import com.google.gson.reflect.TypeToken
 
 internal val gson = Gson()
 
-// Convert an object of Type T to a map of name and value of type V
-internal fun <T, V> T.toMap(): Map<String, V> = convert()
+/**
+ * Convert an object of Type [T] to a map of name and value of type [R]
+ */
+internal fun <T, R> T.toMap(): Map<String, R> = convert()
 
-// Convert an object of type T to type R
-internal inline fun <T, reified R> T.convert(): R {
+/**
+ * Convert an object of type [T] to type [R] using [Gson]
+ */
+private inline fun <T, reified R> T.convert(): R {
     val json = gson.toJson(this)
     return gson.fromJson(json, object : TypeToken<R>() {}.type)
 }
