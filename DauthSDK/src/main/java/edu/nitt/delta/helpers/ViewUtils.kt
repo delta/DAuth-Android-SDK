f2ed9e92b8246efebb49aa3efaa4084c7bba0770
Sudhindra3000 <sudhindra5678@gmail.com>
2021-10-18 15:50:38 +0530

    add Doc comments for ViewUtils.kt

--- DauthSDK/src/main/java/edu/nitt/delta/helpers/ViewUtils.kt
+++ DauthSDK/src/main/java/edu/nitt/delta/helpers/ViewUtils.kt
@@ -8,13 +8,28 @@ import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.util.DisplayMetrics
 
+/**
+ * A collection of utility functions to work with Views
+ */
 internal object ViewUtils {
+
+    /**
+     * Converts length from dp to pixels
+     * @param dp value of length in dp
+     * @param context [Context] object needed to work with Views
+     * @return Given length in pixels
+     */
     fun dpToPixel(dp: Float, context: Context): Float {
         val resources: Resources = context.resources
         val metrics: DisplayMetrics = resources.displayMetrics
         return dp * (metrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)
     }
 
+    /**
+     * Converts a drawable object to Bitmap
+     * @param drawable [Drawable] object
+     * @return The [Bitmap] object created from the given drawable
+     */
     fun drawableToBitmap(drawable: Drawable): Bitmap? {
         if (drawable is BitmapDrawable)
             return drawable.bitmap
